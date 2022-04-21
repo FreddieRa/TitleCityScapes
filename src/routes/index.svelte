4f12dd990da8c718b6a8572329bcfa111fe110ba
Freddie <Freddie.rawlins@mail.com>
2022-04-21 05:06:25 +0100

    Update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,22 @@
-<script context="module">
-	export const prerender = true;
-</script>
-
 <script>
-	import Counter from '$lib/Counter.svelte';
+	import Tesseract from '$lib/Tess.svelte';
+    import Canvas3D from '$lib/Canvas3D.svelte';
+
+    let rects;
 </script>
 
-<svelte:head>
-	<title>Home</title>
-</svelte:head>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
+<main ondragover="return false" class="border border-blue-500 test">
+    <div class="border px-5 grid grid-cols-2 gap-10 justify-items-center h-full max-h-fit">
+        <Tesseract bind:rects/>
+        <Canvas3D bind:rects/>
+    </div>
+</main>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+.test {
+    height: 80vh;
+    width: 100%;
+}
 </style>
