72c3f06630ebeae94908bdec4af6294adfa2bf6e
Freddie <Freddie.rawlins@mail.com>
2022-04-23 17:01:44 +0100

    Version 1.4

---
+++ src/lib/Popup.svelte
@@ -0,0 +1,44 @@
+<script>
+    export let message;
+  </script>
+  
+  <style>
+    h1 {
+          font-size: 2rem;
+          text-align: center;
+      }
+      
+    h2 {
+          font-size: 1.25rem;
+      }
+
+    code {
+          font-size: 1.25rem;
+          /* @apply bg-red-500 px-1 py-1 rounded text-white; */
+      }
+  </style>
+  
+  <main class="p-5">
+  <h1>Welcome to Title City Scapes</h1>
+  <br>
+  
+  <p>Drag an image of an academic title page onto the left section, and hit <code>Process Image</code>. This will run text recognition to try and find all of the words.</p>
+  <br>
+ 
+  <p>When this is done, and there are rectangles around the words, press the <code> Build City </code> button to see those shapes come to life. You can drag to move the 3D model, and scroll to zoom.</p>
+  <br>
+
+  <p>Click the camera icon in the top right to save an image of your city.</p>
+
+  <br>
+
+  <p>If the text recognition doesnt work perfectly, then you can click the <b>+</b> button to draw your own rectangles in, or <b>-</b> button to drag a selection around any boxes you want deleted (this can take a few goes for some reason). Hit enter in both modes to confirm the box!</p>
+
+  <br>
+  
+  <h2>Visit the <b><a href="/about">About</a></b> page to learn more!</h2>
+
+</main>
+  
+
+
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,27 @@
 <script>
-	import Tesseract from '$lib/Tess.svelte';
+	import { onMount } from 'svelte';
+    import { writable } from 'svelte/store';
+    import Modal, { bind } from 'svelte-simple-modal';
+
+    import Tesseract from '$lib/Tess.svelte';
     import Canvas3D from '$lib/Canvas3D.svelte';
+    import Popup from '$lib/Popup.svelte';
+
+    
+    const modal = writable(null);
+    const showModal = () => modal.set(Popup);
 
     let rects;
     let processed;
 
     $ : console.log(processed);
+
+    onMount(() => {
+        const visited = localStorage.getItem('visited', false);
+        console.log(visited)
+        if (!visited) showModal();
+        localStorage.setItem('visited', true);
+    });
 </script>
 
 <svelte:head>
@@ -13,10 +29,12 @@
 </svelte:head>
 
 <main ondragover="return false" class="test">
-    <div class="px-5 grid grid-cols-2 gap-10 justify-items-center h-full max-h-fit">
-        <Tesseract bind:rects bind:processed/>
-        <Canvas3D bind:rects bind:processed/>
-    </div>
+    <Modal show={$modal}>
+        <div class="px-5 grid grid-cols-2 gap-10 justify-items-center h-full max-h-fit">
+            <Tesseract bind:rects bind:processed/>
+            <Canvas3D bind:rects bind:processed/>
+        </div>
+    </Modal>
 </main>
 
 
